7bea33db1d6e271c0dad09abf1e040e67c0e70a3
Anthony Tran <anthonytran.dev@gmail.com>
2018-07-21 17:34:05 +0100

    Merge Conflict

--- src/components/board.js
+++ src/components/board.js
@@ -5,9 +5,6 @@ class Board extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      "board": [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    };
   }
 
   // put in component did mount event?
