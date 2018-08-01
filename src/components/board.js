a8b247910bf1be4204952bcf73df9900438b11f5
Sandbagger <wneal_19@yahoo.co.uk>
2018-08-01 08:00:26 +0100

    added checkLife and countNeighbors functions (not complete)

--- src/components/board.js
+++ src/components/board.js
@@ -5,24 +5,49 @@ import Row from './row';
 var generateBoard = (rowCount, squareCount) => {
   let board = [];
   for (let i =0; i< rowCount; i++){
-    let newRow = {};
+    let newRow = [];
     for (let j = 0; j < squareCount; j++){
+      if(i === 1 && j ===1){
+        newRow[j] = true;
+      }else{
       newRow[j] = false;
-    }
+    }}
     board.push(newRow);
   }
+  console.log('generate board', board)
+  console.log('1,1', board[1][1] )
   return board;
 }
 
 class Board extends Component {
   constructor(props) {
     super(props);
-    this.rowCount = 3;
-    this.squareCount = 3;
+    this.rowCount = 4;
+    this.squareCount = 4;
     this.state = {
       "board": generateBoard(this.rowCount, this.squareCount)
     };
   }
+
+  checkLife(){
+    for (let i = 0; i =< this.rowCount; i++){
+      for (let j = 0; j =< this.squareCount; j++){
+        if (j){
+          j = this.countNeighbors(i, j)
+        }
+      }
+    }
+  }
+
+countNeighbors(row, square){
+var cell = this.state.board[row][square];
+neighbors = [
+             [-1,-1], [-1,0], [-1,1],
+             [0,-1],          [0,1],
+             [1,-1],  [1,0],  [1,1]
+            ]
+neighborsTop = this.state.board[row-1 =< this.rowCount ? row-1 : this.rowCount + 1].slice(square -1 )
+}
 
   render() {
     let rows = [];
