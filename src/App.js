91772ba0e5716eee6c51adaed499ca1f5bc460fc
Anthony Tran <anthonytran.dev@gmail.com>
2018-08-06 15:01:57 +0100

    Feat App.js updated
    This component emulates the logic of 4 x 4 matrix board

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react';
-import Board from './components/board';
-import './App.css';
+import React, { Component, Fragment } from 'react'
+import Board from './components/Board'
+import { activeNodesAdjacent } from './utils/matrix'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      matrix: [
+        [false, true, false, false],
+        [false, true, false, false],
+        [false, true, true, false],
+        [false, true, false, false],
+      ]
+    }
+  }
   render() {
+    const [coordinateX, coordinateY] = [2,1]
+    const matrix = this.state.matrix
+    console.log(activeNodesAdjacent([3, 0], matrix))
     return (
-      <div className="board">
-        <Board />
-      </div>
+      <Fragment>
+        <Board matrix={matrix}/>
+      </Fragment>
     );
   }
 }
