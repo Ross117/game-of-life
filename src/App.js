705c437791f7c94af2b5a0b753e8bb3b0921a7bb
Anthony Tran <anthonytran.dev@gmail.com>
2018-08-15 18:18:20 +0100

    Anthony's most recent developments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React, { Component, Fragment } from 'react'
 import Board from './components/Board'
-import { activeNodesAdjacent } from './utils/matrix'
 
 class App extends Component {
   constructor(props){
     super(props)
     this.state = {
       matrix: [
-        [false, true, false, false],
-        [false, true, false, false],
-        [false, true, true, false],
-        [false, true, false, false],
+        [false, false, false, false, false, false, false, false, false, false,],
+        [false, false, false, false, false, false, false, false, false, false,],
+        [false, false, true, true, false, false, false, false, false, false,],
+        [false, false, true, false, false, false, false, false, false, false,],
+        [false, false, false, false, false, false, false, false, false, false,],
+        [false, false, false, false, false, false, false, false, false, false,],
+        [false, false, false, false, false, false, false, false, false, false,],
+        [false, false, false, false, false, false, false, false, false, false,],
+        [false, false, false, false, false, false, false, false, false, false,],
+        [false, false, false, false, false, false, false, false, false, false,],
       ]
     }
   }
   render() {
     const [coordinateX, coordinateY] = [2,1]
-    const matrix = this.state.matrix
-    console.log(activeNodesAdjacent([3, 0], matrix))
+    const matrix = this.state.matrix;
     return (
       <Fragment>
         <Board matrix={matrix}/>
