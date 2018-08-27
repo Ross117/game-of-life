ae7bb02908f54850b4c3a99722ef63c426646e86
Ross <RossMorran@hotmail.co.uk>
2018-08-27 20:00:59 +0100

    Replace incomplete functions from previous merge (checkLife & countNeighbours) with complete versions. Delete matrix.js file, move code into board.js file.

--- src/components/Board.js
+++ src/components/Board.js
@@ -88,4 +88,35 @@ checkLife(){
   }
 }
 
+const aliveOrDead = (neighbours) => { return !(neighbours < 2) && !(neighbours > 3)}
+
+const nodeActive = (coordinates, matrix) => {
+  const [xCoordinate, yCoordinate] = coordinates
+  return matrix[xCoordinate] && matrix[xCoordinate][yCoordinate] ? matrix[xCoordinate][yCoordinate] : false
+}
+
+const adjacentCoordinates = (node) => {
+  const [xNode, yNode] = node
+ 
+ const relativeAdjacentCoordinates = [
+   [-1, -1], [-1, 0], [-1, 1],
+    [0, -1],          [0, 1],
+     [1,-1], [1,0],   [1,1]
+ ];
+
+ const absoluteCoordinates = relativeAdjacentCoordinates.map(relative =>{
+   let [x,y] = relative;
+  let xAbsolute = x + xNode
+  let yAbsolute = y + yNode 
+  return xAbsolute >= 0 && yAbsolute >= 0 ? [xAbsolute, yAbsolute] : false;
+
+  })
+
+ return absoluteCoordinates.filter(i => i)
+ }
+
+const activeNodesAdjacent = (node, matrix) => {
+  return adjacentCoordinates(node).map(i => nodeActive(i, matrix));
+}
+
 export default Board;
