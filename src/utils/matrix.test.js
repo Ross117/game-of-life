3452d0155d38078850e471d39c9eeadb771c7b28
Sandbagger <wneal_19@yahoo.co.uk>
2018-10-07 09:52:48 +0100

    Green test passed for 'returns Array(3) when [0,0] passed in'. Used 'toEqual' instead of 'toBe' as testing object equality

--- src/utils/matrix.test.js
+++ src/utils/matrix.test.js
@@ -1,16 +1,21 @@
 import { validAdjacentNodes } from "./matrix";
 
-const matrix = [
-    [true, false, false],
-    [false, true, false],
-    [false, false, true]
-  ];
-
-  const squareCoordinate = [0,0]
-
 
 describe('activeNodesAdjacent', () => {
+
+    const matrix = [
+        [true, false, false],
+        [false, true, false],
+        [false, false, true]
+      ];
+    
+      const squareCoordinate = [0,0];
+
     it('can call validAdjacentNodes', () => {
         validAdjacentNodes(squareCoordinate);
+    });
+    
+    it('returns Array(3) when [0,0] passed in', () => {
+        expect(validAdjacentNodes(squareCoordinate)).toEqual([[1, 0], [0,1], [1,1]]);
     })
 });
