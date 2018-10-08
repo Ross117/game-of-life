e46ef56bcf66aebd0020bf1090ad5a737d15c438
Sandbagger <wneal_19@yahoo.co.uk>
2018-10-08 06:52:25 +0100

    Added test to last square of board. Issue identified, there is no upper limit to the coordinates that return adjacent nodes.

--- src/utils/matrix.test.js
+++ src/utils/matrix.test.js
@@ -18,4 +18,12 @@ describe('activeNodesAdjacent', () => {
     it('returns Array(3) when [0,0] passed in', () => {
         expect(validAdjacentNodes(squareCoordinate)).toEqual([[1, 0], [0,1], [1,1]]);
     })
+
+    it('returns Array(8) when [1,1] passed in', () => {
+        expect(validAdjacentNodes([1,1])).toEqual([[0, 0], [1,0], [2,0], [0,1], [2,1], [0,2], [1,2], [2,2]]);
+    })
+
+    it('returns Array(8) when [2,2] passed in', () => {
+        expect(validAdjacentNodes([2,2])).toEqual([[1, 1], [2,1], [1,2]]);
+    })
 });
