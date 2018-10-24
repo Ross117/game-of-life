ad0a7d44de53ada6a1e9bf672b1fee847a6ed955
Ross <RossMorran@hotmail.co.uk>
2018-10-24 19:16:16 +0100

    Refactor createMatrix function

--- src/utils/createMatrix.js
+++ src/utils/createMatrix.js
@@ -1,19 +1,20 @@
 
 const createMatrix = (rows, cols) => {
     const randomInt = () => { return Math.round(Math.random()) }
-    let matrix = []
 
     if ((rows < 10 || rows > 100) || (cols < 10 || cols > 100)) {
       throw "arguments passed either too big or too small"
     }
 
-    for (let i = 1; i <= rows; i++) {
-      let row = []
-      for (let j = 1; j <= cols; j++) {
-        row.push(Boolean(randomInt()))
-      }
-      matrix.push(row)
-    }
+    const matrix = Array(rows)
+      .fill(Array(cols)
+      // need to create a placeholder 3rd dimension of the array, so that the 2nd dimension is mappable and not just empty slots
+      .fill(Array(1)))
+      .map( (r) => {
+        return r.map( () => {
+          return Boolean(randomInt());
+        })
+      })
 
     return matrix
 }
