61f977c0efa6c6e503c063939702bdc81706e1a0
Sandbagger <wneal_19@yahoo.co.uk>
2018-10-30 07:07:01 +0000

    Forgot to return the length of activeNodeAdjacent.

--- src/utils/matrix.js
+++ src/utils/matrix.js
@@ -1,16 +1,17 @@
 const aliveOrDead = (neighbours) => { return !(neighbours < 2) && !(neighbours > 3)}
 
-const activeNodesAdjacent = (squareCoordinate, matrix) => {debugger
-  return validAdjacentNodes(squareCoordinate)
+const activeNodesAdjacent = (squareCoordinate, matrix) => {
+  return validAdjacentNodes(squareCoordinate, matrix)
           .map(adjacentNodeCoorinate => {
             const [x, y] = adjacentNodeCoorinate;
             return matrix[x][y]
           })
-          .filter(i => i);
+          .filter(i => i).length;
 }
 
-const validAdjacentNodes = (node) => {
- 
+const validAdjacentNodes = (node, matrix) => {
+
+ const length = matrix.length; 
   const [xNode, yNode] = node;
 
   const nodeOffsets = [
@@ -23,7 +24,7 @@ const validAdjacentNodes = (node) => {
     let [x,y] = offSet;
    let xAbsolute = x + xNode;
    let yAbsolute = y + yNode;
-   return xAbsolute >= 0 && yAbsolute >= 0 ? [xAbsolute, yAbsolute] : false;
+   return xAbsolute >= 0 && xAbsolute < length-1 && yAbsolute >= 0 && yAbsolute < length-1 ? [xAbsolute, yAbsolute] : false;
    })
  
   return applyOffsets.filter(i => i)
