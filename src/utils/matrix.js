87c1ef0f771d9f0fdd725a795ba584eebd39b4ae
Sandbagger <wneal_19@yahoo.co.uk>
2018-11-02 06:43:44 +0000

    Further code refactor

--- src/utils/matrix.js
+++ src/utils/matrix.js
@@ -1,15 +1,6 @@
 const aliveOrDead = (neighbours) => { return !(neighbours < 2) && !(neighbours > 3)}
 
-const activeNodesAdjacent = (squareCoordinate, matrix) => {debugger
-  return validAdjacentNodes(squareCoordinate, matrix)
-          .map(adjacentNodeCoorinate => {
-            const [x, y] = adjacentNodeCoorinate;
-            return matrix[x][y]
-          })
-          .filter(i => i).length;
-}
-
-const validAdjacentNodes = (node, matrix) => {
+const activeNodesAdjacent = (node, matrix) => {debugger
 
  const length = matrix.length; 
   const [xNode, yNode] = node;
@@ -20,19 +11,18 @@ const validAdjacentNodes = (node, matrix) => {
       [-1, 1], [0,1],   [1, 1]
   ];
  
-  const applyOffsets = nodeOffsets.map(offSet =>{
+  const applyOffsets = nodeOffsets.map(offSet =>{debugger
     let [x,y] = offSet;
    let xAbsolute = x + xNode;
    let yAbsolute = y + yNode;
-   return xAbsolute >= 0 && xAbsolute < length && yAbsolute >= 0 && yAbsolute < length ? [xAbsolute, yAbsolute] : false;
+   return xAbsolute >= 0 && xAbsolute < length && yAbsolute >= 0 && yAbsolute < length ? matrix[xAbsolute][yAbsolute] : false;
    })
  
-  return applyOffsets.filter(i => i)
+  return applyOffsets.filter(i => i).length
   }
 
 
 export {
   activeNodesAdjacent,
   aliveOrDead,
-  validAdjacentNodes,
 }
