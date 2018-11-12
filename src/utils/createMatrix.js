5959549b953d2b2e5facc09db2b9974b05d6b6bb
Ross <RossMorran@hotmail.co.uk>
2018-11-12 23:37:08 +0000

    create an Error object if arguments passed are outside of permitted range

--- src/utils/createMatrix.js
+++ src/utils/createMatrix.js
@@ -1,9 +1,8 @@
-
 const createMatrix = (rows, cols) => {
     const randomInt = () => { return Math.round(Math.random()) }
 
     if ((rows < 10 || rows > 100) || (cols < 10 || cols > 100)) {
-      throw "arguments passed either too big or too small"
+      throw Error("arguments passed either too big or too small")
     }
 
     const matrix = Array(rows)
@@ -11,12 +10,12 @@ const createMatrix = (rows, cols) => {
       .fill(0))
       .map( (r) => {
         return r.map( () => {
-          return Boolean(randomInt());
+          return Boolean(randomInt())
         })
       })
 
     return matrix
 }
 
-export default createMatrix;
+export default createMatrix
 
